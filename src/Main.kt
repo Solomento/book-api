238e65f93ac28a3d0a67a775757f1f63b8f2b82f
SappyJoy <stponomarevw@gmail.com>
2020-07-18 15:26:47 +0500

    Добавлена многопоточность

--- src/Main.kt
+++ src/Main.kt
@@ -6,6 +6,8 @@ import java.io.File
 import java.lang.Exception
 import java.net.URL
 import java.time.LocalDateTime
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 const val ANSI_RED = "\u001B[31m"
 const val ANSI_RESET = "\u001B[0m"
@@ -14,16 +16,22 @@ fun main() {
 
     var timeStart = System.currentTimeMillis()
     var downloadPages: List<Book> = listOf()
+
+    val executorService: ExecutorService = Executors.newFixedThreadPool(30)
+
     val firstLetters = listOf("a", "b", "v", "g", "d", "e", "zh", "z", "i", "j", "k", "l", "m",
         "n", "o", "p", "r", "s", "t", "u", "f", "x", "c", "ch", "sh", "shh", "y", "eh", "yu", "ya")
 
+    // Пробегаем по каждой букве алфавита
     for (letter in firstLetters) {
         val doc: Document = Jsoup.connect("https://royallib.com/authors-$letter.html").get()
         val authorElements: Element = doc.getElementsByAttributeValue("class", "navi").get(0)
 
         for (element in authorElements.child(0).children()) {
+            // Идём по всем столбцам
             for (column in 0..3) {
                 try {
+                    // Выбираем автора
                     for (author in element.child(column).children()) {
                         val authorName: String
                         var authorUrl: String
@@ -48,7 +56,7 @@ fun main() {
                                     val zipElement: Element = zipDoc.select("a:contains(Скачать в формате TXT)").first()
                                     println(book.first)
                                     zipUrl = URL("https:" + zipElement.attr("href"))
-                                    Utils.unpackArchive(zipUrl, File("res"))
+                                    executorService.submit { Utils.unpackArchive(zipUrl, File("res")) }
                                 } catch (e: Exception) {
                                     continue
                                 }
@@ -70,6 +78,10 @@ fun main() {
     println(ANSI_RED + "Execution time: " + ANSI_RESET + "${executionTime / 1000 / 60 / 60} hours ${executionTime / 1000 / 60} minutes ${executionTime / 1000} seconds")
 }
 
+/**
+ * По заданному url страницы автора возвращает ссылки на все книги, которые содеражатся на этой странице.
+ * При этом даже те страницы, с которых невозможно скачать книги.
+ */
 fun getBooksUrls(url: String): List<Pair<String, String>> {
     val booksUrls = mutableListOf<Pair<String, String>>()
 
